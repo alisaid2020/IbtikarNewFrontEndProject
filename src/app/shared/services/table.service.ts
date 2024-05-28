8b3598e9e4290b057773ab2ebbe7842f3c8494f5
aljadcodev04 <168093068+aljadcodev04@users.noreply.github.com>
2024-05-29 02:56:58 +0300

    last update

--- src/app/shared/services/table.service.ts
+++ src/app/shared/services/table.service.ts
@@ -42,7 +42,7 @@ export class TableService {
 
   tableColumns(element: any) {
     this.objectKeys = this.gekKeys(element)?.filter(
-      (el: any) => !/id/gi.test(el)
+      (el: any) => !/Id/gi.test(el)
     );
     this.allColumns = [];
     this.objectKeys?.forEach((el: any) => {
